f8c1e06e61f26603bcffe6bba7751e291e15fd69
unexre <unexre@users.noreply.github.com>
2016-06-29 01:04:43 -0700

    add linter, jasmine to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
-var jshint = require('gulp-jshint');
-var gulp   = require('gulp');
+var gulp = require('gulp');
+var pump = require('pump');
 
-gulp.task("lint", function() {
-    gulp.src("./src/*.js")
-        .pipe(jshint())
-        .pipe(jshint.reporter("default"));
+var jshint = require('gulp-jshint');
+var jasmine = require('gulp-jasmine');
+
+gulp.task("lint", function(cb) {
+    pump([
+        gulp.src("./src/*.js"),
+        jshint(),
+        jshint.reporter("default")
+    ], cb);
 });
 
-gulp.task("test", function() {
-
+gulp.task('test', function (cb) {
+    pump([
+        gulp.src('tests/*'),
+        jasmine()
+    ], cb);
 });
